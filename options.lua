9b623e7be83f49f3d2c75a09f17cd1a8d631c62c
Your Name <you@example.com>
2024-07-14 22:53:06 +0800

    nvim

---
+++ options.lua
@@ -0,0 +1,24 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+o.tabstop = 4
+o.softtabstop = 4
+o.ignorecase = true
+o.cursorline = true
+o.timeout = true
+o.timeoutlen = 250
+o.expandtab = true
+o.linebreak = true
+o.autoread = true
+o.autochdir = true
+o.autowriteall = true
+o.breakindent = true
+o.wrap = true
+o.textwidth = 120
+o.ruler = true
+o.relativenumber = true
+o.number = true
+o.whichwrap = ""
