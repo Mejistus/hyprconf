a4c1ee4386f2d155b4273b2626fed93b13e6f070
mejistus <mejistus@outlook.com>
2024-07-22 17:24:51 +0800

    i3

---
+++ i3_config/i3/start.sh
@@ -0,0 +1,5 @@
+sh /home/mejistus/.config/i3/volume.sh &
+
+exec /home/mejistus/Public/Clash/cfw &
+
+sh /home/mejistus/.config/i3/brightness.sh &
---
+++ i3_config/microsoft-edge/Default/trusted_vault.pb
@@ -0,0 +1,4 @@
+
+
+
+e749fb7c8de9d3b3 cfc417c4dd87e64119ac57372faf8a7e
